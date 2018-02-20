43b2e3286c6ff440a1fe9f3ce1168a9f3b72aca9
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-02-20 21:24:00 +0200

    update(nesting_test.js): add book to author

--- test/nesting_test.js
+++ test/nesting_test.js
@@ -3,8 +3,14 @@ let mongoose = require('mongoose');
 let Author = require('../models/author');
 
 describe('Nesting records', function() {
-    it('creates author', function(done) {
-        
+
+    before(function(done) {
+        mongoose.connection.collections.authors.drop(function() {
+            done();
+        });
+    });
+
+    it('creates author', function(done) {        
         let pat = new Author({
             name: 'Patrick',
             books: [
@@ -14,13 +20,35 @@ describe('Nesting records', function() {
                 }
             ]
         });
-
         pat.save().then(function() {
             Author.findOne({ name: 'Patrick' }).then(function(record) {
                 assert(record.books.length === 1);
                 done();
             });
         });
-
     });
+
+    it('Add a book to the author', function(done) {        
+        let pat = new Author({
+            name: 'Patrick',
+            books: [
+                {
+                    title: 'Name of the Wind',
+                    pages: 500
+                }
+            ]
+        });
+        pat.save().then(function() {
+            Author.findOne({ name: 'Patrick' }).then(function(record) {
+                record.books.push({title: "Wise man's fear", pages: "590" });
+                record.save().then(function() {
+                    Author.findOne({name: "Patrick"}).then(function(record) {
+                        assert(record.books.length === 2);
+                        done();
+                    });
+                });                
+            });
+        });
+    });
+
 });
